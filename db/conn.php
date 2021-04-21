9a815233428707c6f03f102c2026085136af5a4b
okeisrul <82898210+okeisrul@users.noreply.github.com>
2021-04-21 14:50:00 +0100

    Update conn.php

--- db/conn.php
+++ db/conn.php
@@ -1,8 +1,8 @@
 <?php
-    $host='127.0.0.1';
-    $db ='attendance_db';
-    $user ='root';
-    $pass = '';
+    $host='remotemysql';
+    $db ='Ori3RY1zHI';
+    $user ='Ori3RY1zHI';
+    $pass = 'QtWFWwEkvD';
     $charset ='utf8mb4';
 
     $dsn ="mysql:host=$host;dbname=$db;charset=$charset";
